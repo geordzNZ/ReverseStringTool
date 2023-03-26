3d6feacaa8d665069ff682d17fad1321f9b8c29b
GeordzNZ <geordie.king@gmail.com>
2023-03-26 22:47:41 +0100

    Add project files.

---
+++ ReverseStringTool/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ReverseStringTool
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
