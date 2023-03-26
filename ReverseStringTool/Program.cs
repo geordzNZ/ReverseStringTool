ca590cec184945d58bc1132c17152e969f5fa664
GeordzNZ <geordie.king@gmail.com>
2023-03-26 22:50:39 +0100

    read user input

--- ReverseStringTool/Program.cs
+++ ReverseStringTool/Program.cs
@@ -9,6 +9,9 @@
             Console.WriteLine("\t\t  String Reversing Tool");
             Console.WriteLine("==========================================================\n");
 
+            // Retrieve User input
+            Console.Write($"\tPlease enter text to be reversed...\n\t");
+            string textToBeReversed = Console.ReadLine();
         }
     }
 }
